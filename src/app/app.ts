1c4fb2503fd88ab6995890402a32b1f4f81df054
williamdev20 <williamalvs45@gmail.com>
2026-06-04 01:08:48 -0300

    feat: adicionar section da Hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Hero } from './sections/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
