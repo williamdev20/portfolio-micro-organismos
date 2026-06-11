cff5d20213e431add28fb90fa960074a9416a02c
williamdev20 <williamalvs45@gmail.com>
2026-06-11 00:52:10 -0300

    feat: adicionar seção com dados

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Hero } from './sections/hero/hero';
 import { LabPractice } from './sections/lab-practice/lab-practice';
 import { Protozoans } from './sections/protozoans/protozoans';
 import { Fungi } from './sections/fungi/fungi';
+import { Data } from './sections/data/data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero, LabPractice, Protozoans, Fungi],
+  imports: [RouterOutlet, Hero, LabPractice, Protozoans, Fungi, Data],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/sections/data/data.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data',
+  imports: [],
+  templateUrl: './data.html',
+  //styleUrl: './data.css',
+})
+export class Data {}
